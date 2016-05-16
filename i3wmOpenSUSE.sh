0834c8aeff290961c9ab896b401d57f926d59c75
Vinícius Barros Rodrigues <viniciusbrbio@gmail.com>
2016-05-15 21:16:01 -0300

    Update i3wmOpenSUSE.sh

--- i3wmOpenSUSE.sh
+++ i3wmOpenSUSE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "-------"
-echo "INSTALLING PACKGES"
+echo "INSTALLING PACKAGES"
 echo "-------"
 sudo zypper install i3 i3status i3lock dmenu gtk-doc gobject-introspection dh-autoreconf autoconf lxappearance automake libtool glib2-devel feh arandr nm-applet rofi compton gtk-chtheme
 echo "-------"
