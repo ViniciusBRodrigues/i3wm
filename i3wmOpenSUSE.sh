15177804a1a7fed86f7207706f589777111867af
Vinícius Barros Rodrigues <viniciusbrbio@gmail.com>
2016-05-15 21:14:16 -0300

    openSUSE Leap 42.1

---
+++ i3wmOpenSUSE.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "-------"
+echo "INSTALLING PACKGES"
+echo "-------"
+sudo zypper install i3 i3status i3lock dmenu gtk-doc gobject-introspection dh-autoreconf autoconf automake libtool glib2-devel feh arandr nm-applet rofi compton gtk-chtheme
+echo "-------"
+echo "Playerctl should be installed manually"
+echo "-------"
+echo "End"
