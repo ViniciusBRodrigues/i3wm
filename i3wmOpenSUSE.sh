762238360680544288a7ec7db217b34fefeff429
Vinícius Barros Rodrigues <viniciusbrbio@gmail.com>
2016-05-15 21:15:32 -0300

    Update i3wmOpenSUSE.sh

--- i3wmOpenSUSE.sh
+++ i3wmOpenSUSE.sh
@@ -2,7 +2,7 @@
 echo "-------"
 echo "INSTALLING PACKGES"
 echo "-------"
-sudo zypper install i3 i3status i3lock dmenu gtk-doc gobject-introspection dh-autoreconf autoconf automake libtool glib2-devel feh arandr nm-applet rofi compton gtk-chtheme
+sudo zypper install i3 i3status i3lock dmenu gtk-doc gobject-introspection dh-autoreconf autoconf lxappearance automake libtool glib2-devel feh arandr nm-applet rofi compton gtk-chtheme
 echo "-------"
 echo "Playerctl should be installed manually"
 echo "-------"
