4a402f881e31165764004b943be4aa511f564976
Vinícius Barros Rodrigues <viniciusbrbio@gmail.com>
2016-05-17 11:13:58 -0300

    Update i3wmOpenSUSE.sh

--- i3wmOpenSUSE.sh
+++ i3wmOpenSUSE.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 echo "-------"
-echo "INSTALLING PACKAGES"
+echo "INSTALLING I3 AND NECESSARY PACKAGES"
 echo "-------"
-sudo zypper install i3 i3status i3lock dmenu gtk-doc gobject-introspection dh-autoreconf autoconf lxappearance automake libtool glib2-devel feh arandr nm-applet rofi compton gtk-chtheme
+sudo zypper install i3 i3status i3lock dmenu gtk-doc gobject-introspection dh-autoreconf autoconf lxappearance automake libtool glib2-devel feh arandr rofi compton gtk-chtheme libnotify4 wget unzip
 echo "-------"
-echo "Playerctl should be installed manually"
+echo "Installing Playerctl"
+echo "-------"
+wget http://github.com/acrisci/playerctl/archive/master.zip
+unzip master.zip
+cd playerctl-master
+./autogen.sh
+make
+sudo make install
+notify-send "i3 installation complete"
 echo "-------"
 echo "End"
+echo "-------"
